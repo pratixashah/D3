fa6c81ccf502f5c7499195197f4cd577a34af5c8
pratixashah <shah.pratixa@gmail.com>
2021-04-04 17:22:50 -0700

    Set up for the svg

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,5 +1,26 @@
 // @TODO: YOUR CODE HERE!
+// Define SVG area dimensions
+var svgWidth = 960;
+var svgHeight = 660;
 
+// Define the chart's margins as an object
+var chartMargin = {
+  top: 30,
+  right: 30,
+  bottom: 30,
+  left: 30
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+// Select body, append SVG area to it, and set the dimensions
+var svg = d3
+  .select("scatter")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
 
 d3.csv("/D3_data_journalism/assets/data/data.csv").then(function(data)
 {
