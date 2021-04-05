cb19b73d41f3b54838e5332114879a61cb1e5d0d
pratixashah <shah.pratixa@gmail.com>
2021-04-05 16:36:13 -0700

    Setup for axis

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -22,6 +22,10 @@ var svg = d3
   .attr("height", svgHeight)
   .attr("width", svgWidth);
 
+  var chartGroup = svg.append("g")
+  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+
 d3.csv("/D3_data_journalism/assets/data/data.csv").then(function(data)
 {
     console.log(data);
@@ -37,6 +41,33 @@ d3.csv("/D3_data_journalism/assets/data/data.csv").then(function(data)
     let state_list = data.map(data => data.abbr);
 
     console.log("State List:",state_list);
+
+   // scale y to chart height
+    var yScale = d3.scaleLinear()
+                    .domain([0, d3.max(healthcare_list)])
+                    .range([chartHeight, 0]);
+
+    // scale x to chart width
+    var xScale = d3.scaleBand()
+                    .domain(poverty_list)
+                    .range([0, chartWidth])
+                    .padding(0.05);
+
+    // create axes
+    var yAxis = d3.axisLeft(yScale);
+    var xAxis = d3.axisBottom(xScale);
+
+    // set x to the bottom of the chart
+    chartGroup.append("g")
+                .attr("transform", `translate(0, ${chartHeight})`)
+                .call(xAxis);
+
+    // set y to the y axis
+    // This syntax allows us to call the axis function
+    // and pass in the selector without breaking the chaining
+    chartGroup.append("g")
+                .call(yAxis);
+
 }).catch(function(error) {
     console.log(error);
   });
