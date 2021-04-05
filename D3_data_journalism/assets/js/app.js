40c4fabfd439b00ccc518c68d018de2e696a2d58
pratixashah <shah.pratixa@gmail.com>
2021-04-04 17:21:14 -0700

    Getting list of data

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -4,4 +4,18 @@
 d3.csv("/D3_data_journalism/assets/data/data.csv").then(function(data)
 {
     console.log(data);
-});
+
+    let poverty_list = data.map(data => data.poverty);
+
+    console.log("Poverty List:",poverty_list);
+
+    let healthcare_list = data.map(data => data.healthcare);
+
+    console.log("Healthcare List:",healthcare_list);
+
+    let state_list = data.map(data => data.abbr);
+
+    console.log("State List:",state_list);
+}).catch(function(error) {
+    console.log(error);
+  });
