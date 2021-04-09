c4ff2f75037f4a8fc92c9e21e39d019e4cf2019c
pratixashah <shah.pratixa@gmail.com>
2021-04-08 23:55:10 -0700

    Adding multiple labels to axes

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,14 +1,13 @@
 // @TODO: YOUR CODE HERE!
 // Define SVG area dimensions
-var svgWidth = 660;
-var svgHeight = 400;
+var svgWidth = 960;
+var svgHeight = 500;
 
-// Define the chart's margins as an object
 var chartMargin = {
-  top: 30,
-  right: 30,
-  bottom: 50,
-  left: 50
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
 };
 
 // Define dimensions of the chart area
@@ -24,6 +23,48 @@ var svg = d3
 
   var chartGroup = svg.append("g")
   .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+// Initial Params
+var chosenXAxis = "poverty";
+
+// function used for updating circles group with new tooltip
+function updateToolTip(chosenXAxis, circlesGroup) {
+
+  var label;
+
+  if (chosenXAxis === "Poverty") {
+    label = "Poverty";
+  }
+  else if(chosenXAxis === "age"){
+    label = "age";
+  }
+  else {
+    label = "income";
+  }
+
+  var toolTip = d3.tip()
+  .attr("class", "tooltip")
+  .offset([5, -5])
+  .html(function(d) {
+    return (`${d.state}<br>Poverty: ${d.poverty}%<br>Obesity: ${d.obesity}%`);
+  });
+
+  // Step 7: Create tooltip in the chart
+  // ==============================
+  chartGroup.call(toolTip);
+
+  // Step 8: Create event listeners to display and hide the tooltip
+  // ==============================
+  cirlceGroup.on("mouseover", function(data) {
+    toolTip.show(data, this);
+  })
+  // onmouseout event
+  .on("mouseout", function(data, index) {
+    toolTip.hide(data);
+  });
+
+  return circlesGroup;
+}
 
 
 d3.csv("/assets/data/data.csv").then(function(data)
@@ -95,43 +136,97 @@ d3.csv("/assets/data/data.csv").then(function(data)
                 .text(function(d){return d.abbr})
                 .attr("font-size",8);
 
-                // Create axes labels
-    chartGroup.append("text")
-              .attr("transform", "rotate(-90)")
-              .attr("y", 0 - chartMargin.left - 5)
-              .attr("x", 0 - (chartHeight / 2) - 40)
-              .attr("dy", "1em")
-              .attr("class", "axisText")
-              .text("Lacks Healthcare (%)");
-
-    chartGroup.append("text")
-            .attr("transform", `translate(${(svgWidth / 2)-100}, ${chartHeight + chartMargin.top + 10})`)
-            .attr("class", "axisText")
-            .text("In Poverty (%)");
-
-
             // Step 6: Initialize tool tip
     // ==============================
-    var toolTip = d3.tip()
-    .attr("class", "tooltip")
-    .offset([5, -5])
-    .html(function(d) {
-      return (`${d.state}<br>Poverty: ${d.poverty}%<br>Obesity: ${d.obesity}%`);
-    });
+  //   var toolTip = d3.tip()
+  //   .attr("class", "tooltip")
+  //   .offset([5, -5])
+  //   .html(function(d) {
+  //     return (`${d.state}<br>Poverty: ${d.poverty}%<br>Obesity: ${d.obesity}%`);
+  //   });
 
-  // Step 7: Create tooltip in the chart
-  // ==============================
-  chartGroup.call(toolTip);
+  // // Step 7: Create tooltip in the chart
+  // // ==============================
+  // chartGroup.call(toolTip);
 
-  // Step 8: Create event listeners to display and hide the tooltip
-  // ==============================
-  cirlceGroup.on("mouseover", function(data) {
-    toolTip.show(data, this);
-  })
-    // onmouseout event
-    .on("mouseout", function(data, index) {
-      toolTip.hide(data);
-    });
+  // // Step 8: Create event listeners to display and hide the tooltip
+  // // ==============================
+  // cirlceGroup.on("mouseover", function(data) {
+  //   toolTip.show(data, this);
+  // })
+  //   // onmouseout event
+  //   .on("mouseout", function(data, index) {
+  //     toolTip.hide(data);
+  //   });
+
+      // // Create axes labels
+      // chartGroup.append("text")
+      // .attr("transform", "rotate(-90)")
+      // .attr("y", 0 - chartMargin.left - 5)
+      // .attr("x", 0 - (chartHeight / 2) - 40)
+      // .attr("dy", "1em")
+      // .attr("class", "axisText")
+      // .text("Lacks Healthcare (%)");
+
+  // chartGroup.append("text")
+  //   .attr("transform", `translate(${(svgWidth / 2)-100}, ${chartHeight + chartMargin.top + 10})`)
+  //   .attr("class", "axisText")
+  //   .text("In Poverty (%)");
+    
+
+  // Create group for two x-axis labels
+    var xlabelsGroup = chartGroup.append("g")
+                                  .attr("transform", `translate(${svgWidth / 2 - 40}, ${svgHeight - 80})`);
+
+  var lblPoverty = xlabelsGroup.append("text")
+                                    .attr("x", 0)
+                                    .attr("y", 20)
+                                    .attr("value", "poverty") // value to grab for event listener
+                                    .classed("active", true)
+                                    .text("In Poverty (%)");
+
+  var lblAge = xlabelsGroup.append("text")
+                                .attr("x", 0)
+                                .attr("y", 40)
+                                .attr("value", "age") // value to grab for event listener
+                                .classed("inactive", true)
+                                .text("Age (Median)");
+
+  var lblIncome = xlabelsGroup.append("text")
+                          .attr("x", 0)
+                          .attr("y", 60)
+                          .attr("value", "income") // value to grab for event listener
+                          .classed("inactive", true)
+                          .text("Household Income (Median)");
+
+
+    // Create group for two x-axis labels
+    var ylabelsGroup = chartGroup.append("g")
+    .attr("transform", `rotate(-90) translate(${0 - (chartHeight / 2) }, ${ 0 - chartMargin.left - 5})`);
+
+    var lblObese = ylabelsGroup.append("text")
+      .attr("x", 0)
+      .attr("y", 20)
+      .attr("value", "obesity") // value to grab for event listener
+      .classed("active", true)
+      .text("Obese (%)");
+
+    var lblAge = ylabelsGroup.append("text")
+    .attr("x", 0)
+    .attr("y", 40)
+    .attr("value", "smokes") // value to grab for event listener
+    .classed("inactive", true)
+    .text("Smokes (%)");
+
+    var lblIncome = ylabelsGroup.append("text")
+    .attr("x", 0)
+    .attr("y", 60)
+    .attr("value", "healthcare") // value to grab for event listener
+    .classed("inactive", true)
+    .text("Lacks Healthcare (%)");
+
+    // updateToolTip function above csv import
+  var circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
 })
 // .catch(function(error) {
 //     console.log(error);
